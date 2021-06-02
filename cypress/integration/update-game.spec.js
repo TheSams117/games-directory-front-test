ac81af4b1f8ae750c2321f76c5f0aa0255db5b1f
Felipe Castillo <jfcastillo1204@gmail.com>
2021-06-02 13:41:29 -0500

    update test fixed

--- cypress/integration/update-game.spec.js
+++ cypress/integration/update-game.spec.js
@@ -1,14 +1,30 @@
 const { BASE_URL } = require('../utils/constants');
-const gameName = "WarzoneEdited"
+const faker = require('faker').random;
+const gameName = `WarzoneEdited${faker.words(1)}`
 const gameConsole = "PC"
 const gameGenre = "FPS edited"
 const gameImgUrl = "https://www.hd-tecnologia.com/imagenes/articulos/2021/04/El-fin-de-Call-of-Duty-Warzone-sera-este-21-de-abril-segun-Activision.jpg"
 
+const gameNameEdited = `GameEditedOk${faker.words(1)}`
+
 describe("When the user want to update a game", () => {
+    before(() => {
+        cy.visit(BASE_URL);
+        cy.get('#btn-add').click()
+        cy.wait(400);
+        cy.get('#game-name').type(gameName)
+        cy.get('#select-game-console').click()
+        cy.get(`[data-value="${gameConsole}"]`).click()
+        cy.wait(400);
+        cy.get('#game-genre').type(gameGenre)
+        cy.wait(400);
+        cy.get('#game-imgurl').type(gameImgUrl)
+        cy.get('#btn-dialog-edit-add').click()
+    });
     describe("When the user doesn't complete all the fields", () => {
         before(() => {
             cy.visit(BASE_URL);
-            cy.get('#Halo  .MuiCardActions-root  #btn-edit').click()
+            cy.get(`#${gameName} .MuiCardActions-root  #btn-edit`).click()
             cy.wait(400);
             cy.get('#game-name').clear()
             cy.wait(400);
@@ -28,11 +44,11 @@ describe("When the user want to update a game", () => {
     describe("When the user try to update a game succesfully", () => {
         before(() => {
             cy.visit(BASE_URL);
-            cy.get('#Halo  .MuiCardActions-root  #btn-edit').click()
+            cy.get(`#${gameName}  .MuiCardActions-root  #btn-edit`).click()
             cy.wait(400);
             cy.get('#game-name').clear()
             cy.wait(400);
-            cy.get('#game-name').type(gameName)
+            cy.get('#game-name').type(gameNameEdited)
             cy.get('#select-game-console').click()
             cy.get(`[data-value="${gameConsole}"]`).click()
             cy.wait(400);
@@ -46,9 +62,9 @@ describe("When the user want to update a game", () => {
             cy.get('#btn-dialog-edit-add').click()
         });
         it("Then the game should be showed with the right name and console and genre", () => {
-            cy.get(`#${gameName} .MuiCardContent-root .MuiTypography-h5`).should("have.text", gameName)
-            cy.get(`#${gameName} .MuiCardContent-root :nth-child(2)`).should("have.text", gameConsole)
-            cy.get(`#${gameName} .MuiCardContent-root :nth-child(3)`).should("have.text", gameGenre)
+            cy.get(`#${gameNameEdited} .MuiCardContent-root .MuiTypography-h5`).should("have.text", gameNameEdited)
+            cy.get(`#${gameNameEdited} .MuiCardContent-root :nth-child(2)`).should("have.text", gameConsole)
+            cy.get(`#${gameNameEdited} .MuiCardContent-root :nth-child(3)`).should("have.text", gameGenre)
         });
     });
 
